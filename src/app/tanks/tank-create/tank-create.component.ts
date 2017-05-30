8b16a35df6921b9b67af515063642b4fd520a928
Kevin Eanes <kevineanes@mail.fresnostate.edu>
2017-05-29 21:11:39 -0700

    tanks done

--- src/app/tanks/tank-create/tank-create.component.ts
+++ src/app/tanks/tank-create/tank-create.component.ts
@@ -3,7 +3,7 @@ import {TankService} from '../tank.service';
 import {Router} from '@angular/router';
 
 @Component({
-  selector: 'tank-create',
+  selector: 'app-tank-create',
   templateUrl: './tank-create.component.html',
   styleUrls: ['./tank-create.component.css']
 })
@@ -12,10 +12,19 @@ export class TankCreateComponent implements OnInit {
 
   tank:Object;
 
-  constructor() { }
+  constructor(
+    private tankService:TankService,
+    private router:Router
+    ) { }
 
   ngOnInit() {
-   
+    this.tank = {};
+  }
+
+  createTank(tank:Object) {
+    this.tankService.addTank(tank).then((resp) => {
+      this.router.navigate(['/tanks']);
+    });
   }
 
 }
